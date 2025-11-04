0598f1c6a36f5784e2727cb0d67600f4dc17cf0e
wom3 <wom2@student.london.ac.uk>
2025-11-05 01:54:02 +0500

    feat: turned sprayCanTool into a constructor function

--- sketch.js
+++ sketch.js
@@ -4,27 +4,6 @@ var toolbox = null;
 var colourP = null;
 var helpers = null;
 
-//spray can object literal
-sprayCan = {
-  name: "sprayCanTool",
-  icon: "assets/sprayCan.jpg",
-  points: 13,
-  spread: 10,
-  draw: function () {
-    //if the mouse is pressed paint on the canvas
-    //spread describes how far to spread the paint from the mouse pointer
-    //points holds how many pixels of paint for each mouse press.
-    if (mouseIsPressed) {
-      for (var i = 0; i < this.points; i++) {
-        point(
-          random(mouseX - this.spread, mouseX + this.spread),
-          random(mouseY - this.spread, mouseY + this.spread)
-        );
-      }
-    }
-  },
-};
-
 function setup() {
   //create a canvas to fill the content div from index.html
   canvasContainer = select("#content");
@@ -44,7 +23,7 @@ function setup() {
   //add the tools to the toolbox.
   toolbox.addTool(new FreehandTool());
   toolbox.addTool(new LineToTool());
-  toolbox.addTool(sprayCan);
+  toolbox.addTool(new SprayCanTool());
   toolbox.addTool(new mirrorDrawTool());
   background(255);
 }
