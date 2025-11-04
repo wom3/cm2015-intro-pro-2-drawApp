c80589e60f945a2f4821ef127c64517eb7f3ab31
wom3 <wom2@student.london.ac.uk>
2025-11-05 03:48:58 +0500

    feat: added RectangleTool

---
+++ rectangleTool.js
@@ -0,0 +1,32 @@
+function RectangleTool() {
+  this.icon = "assets/rectangleTool.png";
+  this.name = "Rectangle";
+
+  var startMouseX = -1;
+  var startMouseY = -1;
+  var drawing = false;
+
+  this.draw = function () {
+    if (mouseIsPressed) {
+      if (startMouseX == -1) {
+        startMouseX = mouseX;
+        startMouseY = mouseY;
+        drawing = true;
+        loadPixels();
+      } else {
+        updatePixels();
+        var currX = mouseX;
+        var currY = mouseY;
+        var x = Math.min(startMouseX, currX); // x,y should be the top-left corner
+        var y = Math.min(startMouseY, currY);
+        var w = Math.abs(currX - startMouseX); // w,h ought to be positive
+        var h = Math.abs(currY - startMouseY);
+        rect(x, y, w, h);
+      }
+    } else if (drawing) {
+      drawing = false;
+      startMouseX = -1;
+      startMouseY = -1;
+    }
+  };
+}
--- sketch.js
+++ sketch.js
@@ -25,6 +25,7 @@ function setup() {
   toolbox.addTool(new LineToTool());
   toolbox.addTool(new SprayCanTool());
   toolbox.addTool(new mirrorDrawTool());
+  toolbox.addTool(new RectangleTool());
   background(255);
 }
 
